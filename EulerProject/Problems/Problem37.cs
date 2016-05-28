31d8a1d26c7b3cf68871c8a1bf786af0ee272157
TsakyLaptop <TsakyLaptop@TSAKYLAPTOP-PC>
2016-05-28 20:58:15 +0300

    Initial commit with the file

---
+++ EulerProject/Problems/Problem37.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProject.Problems
+{
+    class Problem37 : AbstractProblem, IProblem
+    {
+        public Problem37() : base(37)
+        {
+
+        }
+
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
