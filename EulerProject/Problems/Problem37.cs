0c1ee21ba0ad70cf9508af041ecd3ef11a09628f
TsakyLaptop <TsakyLaptop@TSAKYLAPTOP-PC>
2016-05-29 14:03:11 +0300

    Problem 37 solution

--- EulerProject/Problems/Problem37.cs
+++ EulerProject/Problems/Problem37.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EulerProject.Libraries;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,7 +16,38 @@ namespace EulerProject.Problems
 
         public void Run()
         {
-            throw new NotImplementedException();
+            int found = 0 , sum = 0;
+            int currNumber = 11;
+
+            while (found < 11)
+            {
+                if (IsTruncatablePrime(currNumber))
+                {
+                    found++;
+                    sum += currNumber;
+                }
+                currNumber++;
+            }
+
+            this.result = sum.ToString();
+        }
+
+        private bool IsTruncatablePrime(int currNumber)
+        {
+            string numStr = currNumber.ToString();
+            string subSrtRight = "", subSrtLeft = "";
+            for (int i = 1; i <= numStr.Length; i++ )
+            {
+                subSrtRight = numStr.Substring(numStr.Length - i);
+                if (!Primes.IsPrime(Int32.Parse(subSrtRight)))
+                    return false;
+                subSrtLeft = numStr.Substring(0, i);
+                if (!Primes.IsPrime(Int32.Parse(subSrtLeft)))
+                    return false;
+            }
+
+            Console.WriteLine("Both ways truncatable prime:"+ currNumber);
+            return true;
         }
     }
 }
