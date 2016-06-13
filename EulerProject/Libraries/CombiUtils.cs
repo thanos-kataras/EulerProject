8c4df7bf40b63a48268e37e02d2921a7dfec1c2c
TsakyLaptop <TsakyLaptop@TSAKYLAPTOP-PC>
2016-06-13 21:41:04 +0300

    Solution for problem 43. Added a permutations algorithm for numeric problems.

---
+++ EulerProject/Libraries/CombiUtils.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProject.Libraries
+{
+    class CombiUtils
+    {
+
+        public static List<byte[]> IntCombinations(byte[] byteArray, int length = -1)
+        {
+            List<byte[]> combis = new List<byte[]>();
+            combis.Add(new byte[1] { byteArray[0] });
+            for (int i=1; i< byteArray.Length; i++)
+            {
+                byte newDigit = byteArray[i];
+                List<byte[]> latest = combis.Where(x => x.Length == i).ToList();
+                foreach(byte[] num in latest)
+                {
+                    byte[] newNum = new byte[i+1];
+                    Array.Copy(num, 0, newNum, 1, num.Length);
+                    for (int j=0;j < i+1; j++)
+                    {
+                        if (j == 0)
+                            newNum[0] = newDigit;
+                        else
+                        {
+                            byte inter = newNum[j];
+                            newNum[j] = newNum[j - 1];
+                            newNum[j - 1] = inter;
+                        }
+                        byte[] toAddArray = new byte[i+1];
+                        Array.Copy(newNum, toAddArray,newNum.Length);
+                        combis.Add(toAddArray);
+                    }
+                }
+            }
+
+            if (length == -1)
+                return combis;
+            else
+                return combis.Where(x => x.Length == length).ToList();
+        }
+    }
+}
--- EulerProject/Libraries/MathUtils.cs
+++ EulerProject/Libraries/MathUtils.cs
@@ -39,16 +39,37 @@ namespace EulerProject.Libraries
             return rotations;
         }
 
-        public static bool IsPandigital(Int32 number)
-        {
-            string numberStr = number.ToString();
-            for (int i = 1; i <= numberStr.Length; i++)
+        public static bool IsPandigital(Int64 number, int baseInt = 1)
+        {            
+            long[] digits = IntToArray(number);
+            // Account for 10 digit numbers. The number 10 can't be
+            // part of a pandigital.
+            int length = digits.Length < 10 ? digits.Length : 9;
+            Array.Sort(digits);
+
+            long current = -1, previous = -1;
+            for (int i = 0; i <= length - 1; i++)
             {
-                if (!numberStr.Contains(i.ToString()))
+                current = digits[i];
+                if (i + baseInt != current)
                     return false;
+                if (current == previous)
+                    return false;
+                previous = current;
             }
             return true;
         }
+
+        public static long[] IntToArray(long value)
+        {
+            var numbers = new Stack<long>();
+
+            for (; value > 0; value /= 10)
+                numbers.Push(value % 10);
+
+            return numbers.ToArray();
+        }
+
 
         private static Int32 ConcatDigits(int[] cdigits)
         {
