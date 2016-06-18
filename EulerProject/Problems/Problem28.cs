165664b12a8200648adda256fd074e8d50dd3918
Thanos Kataras <tsaky@hotmail.com>
2016-06-18 21:22:20 +0300

    Initial problem 28 commit.

---
+++ EulerProject/Problems/Problem28.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProject.Problems
+{
+    class Problem28 : AbstractProblem, IProblem
+    {
+        public Problem28() : base(28)
+        {
+        }
+
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
