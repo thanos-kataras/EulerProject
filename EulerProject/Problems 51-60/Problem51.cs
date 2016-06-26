3c1e80cb4a632e94c11f296e7438e5eb0320b0ce
Thanos Kataras <tsaky@hotmail.com>
2016-06-26 21:38:27 +0300

    Changing folder structre for better visibility.

---
+++ EulerProject/Problems 51-60/Problem51.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProject.Problems_51_60
+{
+    class Problem51 : AbstractProblem, IProblem
+    {
+        public Problem51() : base(51)
+        {
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Test 51");
+        }
+    }
+}
