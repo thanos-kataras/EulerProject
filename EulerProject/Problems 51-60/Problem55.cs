8f55fb3bcba2174f63f3d22bd1771f57b361b88c
TsakyLaptop <TsakyLaptop@TSAKYLAPTOP-PC>
2016-07-03 12:58:46 +0300

    Problem 55 and StringUtils reverse.

---
+++ EulerProject/Problems 51-60/Problem55.cs
@@ -0,0 +1,41 @@
+﻿using EulerProject.Libraries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProject.Problems_51_60
+{
+    class Problem55 : AbstractProblem, IProblem
+    {
+        public Problem55() : base(55)
+        {
+        }
+
+        public void Run()
+        {
+            int lynchel = 0;
+            for (int i=1; i<10000;i++)
+            {
+                int tries = 0;
+                BigInteger initial = i;
+                bool isLynchel = true;
+                while (tries < 50)
+                {
+                    BigInteger reverse = BigInteger.Parse(StringUtils.Reverse(initial.ToString()));
+                    initial += reverse;
+                    if (StringUtils.IsPalindrome(initial.ToString()))
+                        isLynchel = false;
+
+                    tries++;
+                }
+                if (isLynchel)
+                    lynchel++;
+            }
+
+            this.result = lynchel.ToString();
+        }
+    }
+}
